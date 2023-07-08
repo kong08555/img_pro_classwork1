1802fbea39a7b81d63ca7a894068e83808981576
kong <kong08555@gmail.com>
2023-07-08 15:30:00 +0700

    circle spot

--- classwork_2/classwork2_1.py
+++ classwork_2/classwork2_1.py
@@ -7,25 +7,25 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 blurred = cv.medianBlur(gray, 25) 
 
-minDist = 50     #ระยะห่างขั้นต่ำระหว่างวงกลม
-param1 = 50      #พารามิเตอร์ที่ใช้ในการกรองเส้นของการตรวจจับวงกลม
-param2 = 43      #พารามิเตอร์ที่กำหนดระดับเชื่อมโยงระหว่างวงกลมที่ตรวจจับแล้ว
-minRadius = 5    #รัศมีวงกลมขั้นต่ำที่ต้องการให้ตรวจจับ
-maxRadius = 100  #รัศมีวงกลมสูงสุดที่ต้องการให้ตรวจจับ
+minDist = 50     # ระยะห่างขั้นต่ำระหว่างวงกลม
+param1 = 50      # พารามิเตอร์ที่ใช้ในการกรองเส้นของการตรวจจับวงกลม
+param2 = 43      # พารามิเตอร์ที่กำหนดระดับเชื่อมโยงระหว่างวงกลมที่ตรวจจับแล้ว
+minRadius = 5    # รัศมีวงกลมขั้นต่ำที่ต้องการให้ตรวจจับ
+maxRadius = 100  # รัศมีวงกลมสูงสุดที่ต้องการให้ตรวจจับ
 
-#ตรวจจับวงกลมโดยใช้ค่าparamiter
+# ตรวจจับวงกลมโดยใช้ค่าพารามิเตอร์
 circles = cv.HoughCircles(blurred, cv.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
 
-#วาดวงกลมที่ตรวจพบ
+# สร้างภาพใหม่เฉพาะจุดศูนย์กลางของวงกลม
+output_img = np.zeros_like(img)
 if circles is not None:
     circles = np.uint16(np.around(circles))
     for i in circles[0,:]:
         center = (i[0], i[1])
-        cv.circle(img, center, 3, (255, 0, 0), -1)
+        cv.circle(output_img, center, 2, (300, 0, 4), -1)
         
-        
-cv.imshow('Modified Image with Circles', img)
-cv.imwrite('classwork_2/center_spot_circle.png',img)
+cv.imshow('center_spot', output_img)
+cv.imwrite('classwork_2/center_spot_circle.png', output_img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
--- classwork_2/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2 as cv
-
-# อ่านภาพจากไฟล์
-img = cv.imread('classwork_2/center_spot_circle.png')
-
-cv.imshow('Circle', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
